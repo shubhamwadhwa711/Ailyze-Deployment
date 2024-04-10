546136604f911d4c6fd135247a08ef4202d82fdc
Ayush Kumar <shubhamwadhwa711@gmail.com>
2024-04-10 16:55:02 +0530

    fix: removed database container

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 docker compose \
     -f docker-compose.yml \
-    -f ./compose-files/others/docker-compose.db.yml \
     -f ./compose-files/backend-chatbot/docker-compose.chatbot.dev.yml \
     -f ./compose-files/backend-oneclickinsight/docker-compose.oneclickinsight.dev.yml \
-    -f ./compose-files/others/docker-compose.db.yml \
     -f ./compose-files/others/docker-compose.celery.yml \
     -f ./compose-files/others/docker-compose.redis.yml \
     -f ./compose-files/others/docker-compose.rabbitmq.yml \
     "$@";
-
-    # -f ./compose-files/nginx/docker-compose.nginx.yml \
-    #   -f ./compose-files/frontend/docker-compose.frontend.dev.yml \
